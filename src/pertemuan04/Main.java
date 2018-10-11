94d3fc6a63900f927043e42912de2a95ff02002d
Roby <robyeka@email.unikom.ac.id>
2018-10-11 22:38:12 +0700

    First commit

---
+++ src/pertemuan04/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan04;
+
+/**
+ *
+ * @author eka
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Member objMember = new Member();
+        objMember.setNama("Rizki Adam Kurniawan");
+        System.out.println("Nama : " +objMember.getNama());
+        
+    }
+    
+}
